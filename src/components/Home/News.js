accfc8a260d205f075ab7a606709ff536d1d6286
nnorbert19 <norb0919@gmail.com>
2022-11-01 12:08:43 +0100

    working carousel with react slick

--- src/components/Home/News.js
+++ src/components/Home/News.js
@@ -2,13 +2,52 @@ import React from "react";
 import "./Carousel.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Slider from "react-slick";
+
+import "./slick.css";
+import "./slick-theme.css";
+
 function News() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  };
   return (
     <>
-      <div classNameName="container">
-        <div classNameName="row text-center">1231</div>
-        <div classNameName="row text-center">22231</div>
-        <div classNameName="row text-center">22231</div>
+      <div classNameName="">
+        <h2> Multiple items </h2>
+        <Slider {...settings} className="">
+          <div>
+            <h1>1</h1>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+          <div>
+            <h3>7</h3>
+          </div>
+          <div>
+            <h3>8</h3>
+          </div>
+          <div>
+            <h3>9</h3>
+          </div>
+        </Slider>
       </div>
     </>
   );
