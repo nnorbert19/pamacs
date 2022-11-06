fb5bf1a16f8452a0f628a9aacaf203eda94f22c9
nnorbert19 <norb0919@gmail.com>
2022-11-06 20:18:20 +0100

    .

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useContenful } from "../ContentfulContext";
 import NewsListerComponent from "../components/News/NewsListerComponent";
 
